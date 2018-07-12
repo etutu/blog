0abc0a12e8bcd3b85dbc03ec7a11c215a860fd2e
chen <chen@chen.com>
2018-07-12 15:38:22 +0800

    修改url.py实现网络访问

--- learn/views.py
+++ learn/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+
+# coding:utf-8
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse(u"欢迎光临 自强学堂!")
